a7b909d2c04165f4aa4f158bd2d8949258320a13
rajeevbh <rajeevbh15290@gmail.com>
2023-07-19 15:27:48 -0400

    Software Install Script - First Commit

--- Practice.ps1
+++ Practice.ps1
@@ -1,3 +1,11 @@
+# Script to install sosftware using powershell 
+
+$32bit_softwares = Get-ItemProperty HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+                Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
 
 
-Get-Credential
+$64bit_softwares = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* |
+                Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+
+$all_softwares = $32bit_softwares + $64bit_softwares
+
