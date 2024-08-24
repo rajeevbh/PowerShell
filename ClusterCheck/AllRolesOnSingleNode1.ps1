36f3f8a99009abb05eb31272b1d663c1e932f2d5
Rajeev Bhardwaj <57496825+rajeevbh@users.noreply.github.com>
2024-08-24 06:15:34 +0530

    Update AllRolesOnSingleNode1.ps1

--- ClusterCheck/AllRolesOnSingleNode1.ps1
+++ ClusterCheck/AllRolesOnSingleNode1.ps1
@@ -2,18 +2,27 @@
 #Get-Cluster | Select-Object -ExpandProperty Name
 $clusterName = "TestCluster"
 
+# Get 3 leter of hostname 
+$hostnamePrefix = ($env:COMPUTERNAME).Substring(0, 3)
+Write-Host "$hostnameprefix"
+
 # Get all cluster groups (roles)
-$clusterGroups = Get-ClusterGroup -Cluster $clusterName
+$clusterGroups = Get-ClusterGroup | Where-Object { $_.Name -like "*ROLE*" }
+write-host "$clusterGroups"
+
+# Get all cluster groups (roles)
+# $clusterGroups= Get-ClusterGroup -Cluster $clusterName
 
 # Get the owner nodes of all groups
 $ownerNodes = $clusterGroups | Select-Object -ExpandProperty OwnerNode | Select-Object -ExpandProperty Name
+Write-Host "$ownerNodes"
 
 # Get the unique nodes from the list of owner nodes
 $uniqueNodes = $ownerNodes | Sort-Object | Get-Unique
 
 # Check if all roles are running on a single node
 if ($uniqueNodes.Count -eq 1) {
-    Write-Output "All cluster roles are running on a single node: $($uniqueNodes[0])"
+    Write-Output "All cluster roles are running on a single node: $($uniqueNodes)"
 } else {
     Write-Output "Cluster roles are distributed across multiple nodes."
     Write-Output "Roles per node:"
