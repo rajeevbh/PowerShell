1bb4cf926d58f15b9e864cdd0e6d2135ee9e8750
Rajeev Bhardwaj <57496825+rajeevbh@users.noreply.github.com>
2024-09-30 15:15:01 -0400

    Update ClusterCheckFinal.ps1

--- ClusterCheck/ClusterCheckFinal.ps1
+++ ClusterCheck/ClusterCheckFinal.ps1
@@ -1,17 +1,13 @@
-# Define the cluster name
-#Get-Cluster | Select-Object -ExpandProperty Name
-$clusterName = "TestCluster"
+#Get the cluster name
+$clusterName = Get-Cluster | Select-Object -ExpandProperty Name
 
-# Get 4 leter of hostname 
-$hostnamePrefix = ($env:COMPUTERNAME).Substring(0, 4)
-Write-Host "$hostnameprefix"
+# Get first four leter of cluster name as role name is aligned with it 
+$clusterNamePrefix = $clusterName.Substring(0, 4)
+Write-Host "$clusterNamePrefix"
 
 # Get all cluster groups (roles)
-$clusterGroups = Get-ClusterGroup | Where-Object { $_.Name -like "*ROLE*" }
+$clusterGroups = Get-ClusterGroup | Where-Object { $_.Name -like "*$clusterNamePrefix*" }
 write-host "$clusterGroups"
-
-# Get all cluster groups (roles)
-# $clusterGroups= Get-ClusterGroup -Cluster $clusterName
 
 # Get the owner nodes of all groups
 $ownerNodes = $clusterGroups | Select-Object -ExpandProperty OwnerNode | Select-Object -ExpandProperty Name
@@ -25,8 +21,5 @@ if ($uniqueNodes.Count -eq 1) {
     Write-Output "All cluster roles are running on a single node: $($uniqueNodes)"
 } else {
     Write-Output "Cluster roles are distributed across multiple nodes."
-    Write-Output "Roles per node:"
-    $ownerNodes | Group-Object | ForEach-Object {
-        Write-Output "$($_.Name): $($_.Count)"
     }
 }
