ba7fad03ffbb7c1387ea811bc8324b448ad56a0b
Rajeev Bhardwaj <57496825+rajeevbh@users.noreply.github.com>
2025-02-20 10:47:01 +0530

    Delete Get_Content.ps1

--- Get_Content.ps1
+++
@@ -1,8 +0,0 @@
-# Get-Content command to used to read the content of a file
-
-Get-Content .\test.txt # Read data from current folder
-
-Get-Content -Path C:\DevOps\PowerShell\test.txt
-
-$data = Get-Content -Path C:\DevOps\PowerShell\test.txt
-Write-Output $data
